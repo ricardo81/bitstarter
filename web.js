61a50754f1928d0d94e73da3453b39be8fc68730
ricardo81 <rn_silva@sapo.pt>
2013-07-10 12:30:20 -0700

    Added index.html

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-
+var text =new Buffer(20);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send((fs.readFileSync("index.html",null)).toString('utf8',0,text.length);
+    text.copy(fs.readFileSync('index.html',null),20);
+    response.send(text.toString('utf8',0,text.length));
 });
 
 var port = process.env.PORT || 5000;
